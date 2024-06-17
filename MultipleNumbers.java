fc6d0224638defb7c5e174ef2f7f4558b3800fdd
lordIguthu <2301942@students.kcau.ac.ke>
2024-06-17 18:55:56 +0300

    Add files via upload

--- MultipleNumbers.java
+++ MultipleNumbers.java
@@ -12,11 +12,11 @@ public class MultipleNumbers {
         System.out.println("Multiples of 2, 3, and 7 from 71 to 150:");
 
 //Loops through the range from 71 to 150
-        for (int i = 71; i <= 150; i++) {
+        for (int rangedigit = 71; rangedigit <= 150; rangedigit++) {
 
 
 //Checks if the number is a multiple of 2, 3, or 7
-            if (i % 2 == 0 || i % 3 == 0 || i % 7 == 0) {
+            if (rangedigit % 2 == 0 || rangedigit % 3 == 0 || rangedigit % 7 == 0) {
 
 //Prints out the number
                 System.out.println(i);
